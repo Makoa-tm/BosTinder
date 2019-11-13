c1ee6c9d55e894c5649e33e099a0e5fab5b61419
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2019-11-13 07:31:53 -0500

    Update InterfazInicio.java

--- src/vista/InterfazInicio.java
+++ src/vista/InterfazInicio.java
@@ -1,43 +1,81 @@
 package vista;
 
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import controlador.Controlador;
 
 public class InterfazInicio extends JFrame {
-	private JLabel fondo;
+	private JLabel fondo_2;
+	private JTextField txtUser;
+	private JTextField txtPassword;
+	private JButton btn;
 
 	public InterfazInicio(Controlador control) {
-		setSize(420, 383);
+		setSize(419, 435);
 		setResizable(true);
 		setTitle("BosTinder");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 470, 407);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setSize(new Dimension(20, 30));
-		lblNewLabel.setPreferredSize(new Dimension(15, 14));
-		lblNewLabel.setIcon(new ImageIcon("D:\\Local files\\java-2018-12\\eclipse\\Workspace\\BosTinder\\src\\images\\Icon image 3.png"));
-		lblNewLabel.setBounds(-27, -33, 524, 474);
-		panel.add(lblNewLabel);
-		
-		fondo = new JLabel("");
-		fondo.setIcon(new ImageIcon(InterfazInicio.class.getResource("/images/Blue Background_2.jpg")));
-		fondo.setBounds(0, -27, 420, 405);
-		panel.add(fondo);
 
-		
+		btn = new JButton("New button");
+		btn.setBounds(138, 312, 136, 30);
+		panel.add(btn);
+
+		txtPassword = new JTextField();
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(121, 236, 171, 28);
+		panel.add(txtPassword);
+
+		JLabel passLogo = new JLabel("");
+		passLogo.setIcon(new ImageIcon(InterfazInicio.class.getResource("/images/Password icon_2.png")));
+		passLogo.setBounds(75, 235, 29, 30);
+		panel.add(passLogo);
+
+		txtUser = new JTextField();
+		txtUser.setBounds(121, 187, 171, 28);
+		panel.add(txtUser);
+		txtUser.setColumns(10);
+
+		JLabel icon_logo = new JLabel("");
+		icon_logo.setIcon(new ImageIcon(InterfazInicio.class.getResource("/images/Icon image 3_.png")));
+		icon_logo.setBounds(74, 185, 30, 37);
+		panel.add(icon_logo);
+
+		JLabel lblBostinderLogin = new JLabel("BosTinder Login");
+		lblBostinderLogin.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+		lblBostinderLogin.setBounds(130, 130, 156, 37);
+		panel.add(lblBostinderLogin);
+
+		JLabel logo = new JLabel("");
+		logo.setIcon(new ImageIcon(InterfazInicio.class.getResource("/images/Icon image 3.png")));
+		logo.setSize(new Dimension(20, 30));
+		logo.setPreferredSize(new Dimension(15, 14));
+		logo.setBounds(141, 11, 121, 114);
+		panel.add(logo);
+
+		fondo_2 = new JLabel("");
+		fondo_2.setIcon(new ImageIcon(InterfazInicio.class.getResource("/images/Blue Background_2.jpg")));
+		fondo_2.setBounds(-5, -18, 620, 350);
+		panel.add(fondo_2);
+
+		JLabel Fondo_1 = new JLabel("");
+		Fondo_1.setIcon(new ImageIcon(InterfazInicio.class.getResource("/images/Blue Background_2.jpg")));
+		Fondo_1.setBounds(-7, 166, 620, 350);
+		panel.add(Fondo_1);
 
 	}
 }
