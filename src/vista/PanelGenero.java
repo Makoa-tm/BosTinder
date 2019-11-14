87f8b0dbe8dd27ab5ab34257930d3f949440a19e
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2019-11-14 15:52:58 -0500

    Cambios en el panelGenero

--- src/vista/PanelGenero.java
+++ src/vista/PanelGenero.java
@@ -7,18 +7,21 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class PanelGenero extends JFrame {
 	private JPanel contenedor;
-	private JButton btnHombre;
 	private JButton btnMujer;
+	private JLabel fondo;
+	private JButton btnHombre;
 
 	public PanelGenero() {
 		setResizable(false);
 		setTitle("BosTinder");
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
 		setLocationRelativeTo(null);
-		setSize(432, 275);
+		setSize(431, 276);
 		getContentPane().setLayout(null);
 
 		contenedor = new JPanel();
@@ -26,29 +29,41 @@ public class PanelGenero extends JFrame {
 		getContentPane().add(contenedor);
 		contenedor.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 13));
-		lblNewLabel.setBounds(146, 11, 143, 25);
-		contenedor.add(lblNewLabel);
-
-		btnHombre = new JButton("Hombre");
-		btnHombre.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 12));
-		btnHombre.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnHombre = new JButton("");
+		btnHombre.setIcon(new ImageIcon(PanelGenero.class.getResource("/images/Men.png")));
 		btnHombre.setActionCommand("HOMBRE");
-		btnHombre.setBounds(20, 65, 172, 167);
+		btnHombre.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnHombre.setBounds(53, 74, 140, 140);
 		contenedor.add(btnHombre);
 
-		btnMujer = new JButton("Mujer");
+		JLabel labelGenero = new JLabel("");
+		labelGenero.setBackground(new Color(255, 255, 255));
+		labelGenero.setForeground(new Color(255, 255, 255));
+		labelGenero.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 13));
+		labelGenero.setBounds(146, 11, 140, 25);
+		contenedor.add(labelGenero);
+
+		btnMujer = new JButton("");
+		btnMujer.setIcon(new ImageIcon(PanelGenero.class.getResource("/images/Women.png")));
 		btnMujer.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 13));
 		btnMujer.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 		btnMujer.setActionCommand("MUJER");
-		btnMujer.setBounds(231, 64, 172, 167);
+		btnMujer.setBounds(241, 74, 140, 140);
 		contenedor.add(btnMujer);
-		
+
 		JLabel lblDinosTuGenero = new JLabel("Dinos tu genero!");
 		lblDinosTuGenero.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 13));
 		lblDinosTuGenero.setBounds(146, 11, 140, 25);
 		contenedor.add(lblDinosTuGenero);
+
+		fondo = new JLabel("");
+		fondo.setIcon(new ImageIcon(PanelGenero.class.getResource("/images/elegant-red-background_1340-4770.jpg")));
+		fondo.setBounds(0, 0, 426, 246);
+		contenedor.add(fondo);
 		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		PanelGenero pGenero = new PanelGenero();
 	}
 }
