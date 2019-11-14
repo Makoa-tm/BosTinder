dce23e2c3a5df5f2ab54f0fcfa9a86bb83ae34bc
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2019-11-14 16:54:41 -0500

    Actualizacion 14/11/19

--- src/vista/InterfazAdmin.java
+++ src/vista/InterfazAdmin.java
@@ -2,12 +2,13 @@ package vista;
 
 import javax.swing.JFrame;
 
-import controlador.Controlador;
-
 public class InterfazAdmin extends JFrame {
 
-	public InterfazAdmin(Controlador control) {
+	public InterfazAdmin() {
 
 	}
 
+	public static void main(String[] args) {
+		InterfazAdmin iAdmin = new InterfazAdmin();
+	}
 }
